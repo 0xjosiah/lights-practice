54bfd3818c969530a38fa9c221fe994e0d77afa9
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-09-28 18:00:15 -0600

    changes material to standard, adds spotLight

--- main.js
+++ main.js
@@ -3,20 +3,60 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as dat from 'lil-gui'
 
-
+/**
+ * Base
+ */
+// Debug GUI
 const gui = new dat.GUI()
 gui.add(gui, 'reset')
 
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+// Scene
 const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight({
+  color: 0xffffff,
+  intensity: .4
+})
+scene.add(ambientLight)
+
+const addHelperToGui = (value, helperObj) => {
+  const {help} = helperObj
+  value ? scene.add(help) : scene.remove(help)
+  window.requestAnimationFrame(() => spotLightHelper.help.update())
+}
+
+const spotLight = new THREE.SpotLight(0xffffff, .5, 10, Math.PI * .1, .05, 1)
+spotLight.position.y = 1
+scene.add(spotLight)
+// spotLight.target.position.x = -.2
+scene.add(spotLight.target)
+const spotLightFolder = gui.addFolder('Spot')
+spotLightFolder.add(spotLight, 'intensity', 0, 1, .001)
+spotLightFolder.add(spotLight, 'distance', 0, 100, 1)
+spotLightFolder.add(spotLight, 'angle', 0, Math.PI, .001)
+spotLightFolder.add(spotLight, 'penumbra', 0, 1, .001)
+spotLightFolder.add(spotLight, 'decay', 0, 5, .01)
+const spotLightHelper = {
+  help: new THREE.SpotLightHelper(spotLight),
+  helper: false
+}
+spotLightFolder.add(spotLightHelper, 'helper').onChange(v => addHelperToGui(v, spotLightHelper))
+window.requestAnimationFrame(() => spotLightHelper.help.update())
+// scene.add(spotLightHelper)
 
 /**
  * Objects
  */
 // Material
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-// material.roughness = .4
+const material = new THREE.MeshStandardMaterial({color: 0xff0000})
+material.roughness = .4
+material.metalness = .8
 
 // Object
 const sphere = new THREE.Mesh(
