d94bd7af337774c8bf192fa73229c42f755e939c
Josiah Webb <0xjosiah@Josiahs-MacBook-Pro.local>
2022-09-29 19:22:01 -0600

    adds min/maxDistance for controls

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ const floor = new THREE.Mesh(
   material// new THREE.MeshPhongMaterial({ color: 0xffffff })
 )
 floor.rotation.x = - Math.PI * .5
-floor.position.y = -1
+floor.position.y = -1.5
 floor.receiveShadow = true
 
 const room = new THREE.Mesh(
@@ -195,14 +195,16 @@ window.addEventListener('resize', () => {
  */
 // Base Cam
 const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 100 );
-// camera.position.x = 1
-// camera.position.y = 1
-camera.position.z = 3
+camera.position.x = -1
+camera.position.y = 2
+camera.position.z = 6
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+controls.minDistance = 2
+controls.maxDistance = 10
 
 /**
  * Renderer
