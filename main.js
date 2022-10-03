8be2aec321e807f964d1c7518aaa11b2d0bd5b9a
Josiah Webb <jj.webb520@gmail.com>
2022-10-03 17:55:12 -0600

    clean up of comments, refactors centerObj

--- main.js
+++ main.js
@@ -35,23 +35,6 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight({
-  color: 0xffffff,
-  intensity: .2
-})
-// scene.add(ambientLight)
-
-// Helper Functions
-const addHelperToGui = (value, helperObj) => {
-  const {help} = helperObj
-  value ? scene.add(help) : scene.remove(help)
-  window.requestAnimationFrame(() => spotLightHelper.help.update())
-}
-
-const updateHelper = helper => {
-  window.requestAnimationFrame(() => helper.help.update())
-}
-
 // Spot Lights
 const spotLight = new THREE.SpotLight(0xff0000, 1, 10, Math.PI * .3, .5, 0)
 spotLight.position.x = 0
@@ -59,23 +42,6 @@ spotLight.position.y = 5
 spotLight.position.z = 2.5
 scene.add(spotLight)
 scene.add(spotLight.target)
-// const spotLightFolder = gui.addFolder('Spot 1')
-// spotLightFolder.add(spotLight, 'intensity', 0, 1, .001).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight, 'distance', 0, 100, 1).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight, 'angle', 0, Math.PI, .001).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight, 'penumbra', 0, 1, .001).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight, 'decay', 0, 5, .01).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.addColor(spotLight, 'color').onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight.position, 'x', -5, 5, .25).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight.position, 'y', -5, 5, .25).onChange(() => updateHelper(spotLightHelper))
-// spotLightFolder.add(spotLight.position, 'z', -5, 5, .25).onChange(() => updateHelper(spotLightHelper))
-const spotLightHelper = {
-  help: new THREE.SpotLightHelper(spotLight),
-  helper: false
-}
-// spotLightFolder.add(spotLightHelper, 'helper').onChange(v => addHelperToGui(v, spotLightHelper))
-// updateHelper(spotLightHelper)
-// scene.add(spotLightHelper.help)
 
 const spotLight2 = new THREE.SpotLight(0x00ff00, 1, 10, Math.PI * .3, .5, 0)
 spotLight2.position.x = 1.8
@@ -83,23 +49,6 @@ spotLight2.position.y = 5
 spotLight2.position.z = -1.8
 scene.add(spotLight2)
 scene.add(spotLight2.target)
-// scene.add(new THREE.CameraHelper(spotLight2.shadow.camera))
-// const spotLightFolder2 = gui.addFolder('Spot 2')
-// spotLightFolder2.add(spotLight2, 'intensity', 0, 1, .001).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2, 'distance', 1, 20, 1).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2, 'angle', 0, 1, .001).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2, 'penumbra', 0, 1, .001).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2, 'decay', 0, 5, .01).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.addColor(spotLight2, 'color').onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2.position, 'x', -5, 5, .25).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2.position, 'y', -5, 5, .25).onChange(() => updateHelper(spotLightHelper2))
-// spotLightFolder2.add(spotLight2.position, 'z', -5, 5, .25).onChange(() => updateHelper(spotLightHelper2))
-const spotLightHelper2 = {
-  help: new THREE.SpotLightHelper(spotLight2),
-  helper: false
-}
-// spotLightFolder2.add(spotLightHelper2, 'helper').onChange(v => addHelperToGui(v, spotLightHelper2))
-// scene.add(spotLightHelper2.help)
 
 const spotLight3 = new THREE.SpotLight(0x0000ff, 1, 10, Math.PI * .3, .5, 0)
 spotLight3.position.x = -1.8
@@ -107,24 +56,6 @@ spotLight3.position.y = 5
 spotLight3.position.z = -1.8
 scene.add(spotLight3)
 scene.add(spotLight3.target)
-// scene.add(new THREE.CameraHelper(spotLight3.shadow.camera))
-// const spotLightFolder3 = gui.addFolder('Spot 3')
-// spotLightFolder3.add(spotLight3, 'intensity', 0, 1, .001).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3, 'distance', 0, 100, 1).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3, 'angle', 0, Math.PI, .001).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3, 'penumbra', 0, 1, .001).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3, 'decay', 0, 5, .01).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.addColor(spotLight3, 'color').onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3.position, 'x', -5, 5, .25).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3.position, 'y', -5, 5, .25).onChange(() => updateHelper(spotLightHelper3))
-// spotLightFolder3.add(spotLight3.position, 'z', -5, 5, .25).onChange(() => updateHelper(spotLightHelper3))
-const spotLightHelper3 = {
-  help: new THREE.SpotLightHelper(spotLight3),
-  helper: false
-}
-// spotLightFolder3.add(spotLightHelper3, 'helper').onChange(v => addHelperToGui(v, spotLightHelper3))
-// updateHelper(spotLightHelper3)
-// scene.add(spotLightHelper3.help)
 
 // Bottom Spot Light
 const bottomSpot = new THREE.SpotLight(0xffffff, 1, 5, Math.PI * .6, 0.75, .5)
@@ -132,13 +63,11 @@ bottomSpot.castShadow = true
 bottomSpot.position.y = -2
 scene.add(bottomSpot)
 scene.add(bottomSpot.target)
-// scene.add(new THREE.SpotLightHelper(bottomSpot))
 bottomSpot.shadow.mapSize.width = 1024
 bottomSpot.shadow.mapSize.height = 1024
 bottomSpot.shadow.camera.fov = 50
 bottomSpot.shadow.camera.near = .5
 bottomSpot.shadow.camera.far = 5
-// scene.add(new THREE.CameraHelper(bottomSpot.shadow.camera))
 
 /**
  * Objects
@@ -147,16 +76,13 @@ bottomSpot.shadow.camera.far = 5
 const material = new THREE.MeshStandardMaterial()
 material.roughness = .4
 material.metalness = .8
-const materialFolder = gui.addFolder('Material')
+const materialFolder = gui.addFolder('Adjust Material')
 materialFolder.add(material, 'roughness', 0, 1, .001)
 materialFolder.add(material, 'metalness', 0, 1, .001)
 
 // Container or 'room'
 const room = new THREE.Mesh(
-  // new THREE.BoxGeometry(20, 20, 20),
   new THREE.SphereGeometry(10),
-  // new THREE.OctahedronGeometry(15),
-  // new THREE.MeshPhongMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide })
   material
 )
 room.material.side = THREE.DoubleSide
@@ -166,16 +92,15 @@ room.castShadow = true
 // Objects in scene
 // Geometries
 const sphere = new THREE.SphereGeometry(1, 64, 64);
-const octo = new THREE.OctahedronGeometry();
+const octahedron = new THREE.OctahedronGeometry();
 const torusKnot = new THREE.TorusKnotGeometry(1, .3, 300, 200, 2, 3);
-const dodeca = new THREE.DodecahedronGeometry()
-const pill = new THREE.CapsuleGeometry(.75, 1, 64, 64)
+const dodecahedron = new THREE.DodecahedronGeometry()
+const capsule = new THREE.CapsuleGeometry(.75, 1, 64, 64)
 const cube = new THREE.BoxGeometry()
 
 // Mesh
-const mesh = new THREE.Mesh( octo, material )
+const mesh = new THREE.Mesh( octahedron, material )
 mesh.castShadow = true
-// mesh.receiveShadow = true
 
 scene.add(mesh, room)
 
@@ -194,12 +119,12 @@ colorFolder.add(colorObj, 'scheme', ['monochrome', 'monochrome-dark', 'monochrom
   .onChange(v => {
     colorObj.scheme = v
     fetchColorData(colorObj)
-  });
+});
 colorFolder.addColor(colorObj, 'color').onChange(v => {
   colorObj.color = v
   spotLight.color = new THREE.Color(colorObj.color)
   fetchColorData(colorObj)
-})
+});
 
 function fetchColorData(colorObj) {
   let {scheme, color} = colorObj
@@ -215,20 +140,18 @@ function fetchColorData(colorObj) {
 
 //Change Object shape
 let centerObj = {
-  shape: octo,
+  shape: octahedron,
   shapes: {
-    octahedron: octo,
-    sphere: sphere,
-    torus_knot: torusKnot,
-    dodecahedron: dodeca,
-    capsule: pill,
+    octahedron,
+    sphere,
+    torusKnot,
+    dodecahedron,
+    capsule,
     cube
   }
 }
-console.log(centerObj);
 
 const centerObjFolder = gui.addFolder('Select Shape')
-console.log(centerObj.shape);
 centerObjFolder.add(centerObj, 'shape', centerObj.shapes).onChange(v => {
   centerObj.shape = v
   mesh.geometry = centerObj.shape
@@ -289,24 +212,6 @@ const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
-
-  // Update objects
-  // octo.rotation.y = elapsedTime * .5
-  // octo.rotation.z = elapsedTime * .5
-
-  // // Update Spots
-  // spotLight.position.y = Math.sin(elapsedTime) * 1.5
-  // spotLight.position.z = Math.cos(elapsedTime) * 1.5
-  // // spotLight.intensity = Math.abs(Math.sin(elapsedTime)) + .5
-  // // spotLight.position.z = Math.sin(elapsedTime) * 1.5
-  // spotLight2.position.y = Math.sin(elapsedTime) * 1.5 + .25
-  // spotLight2.position.z = Math.cos(elapsedTime) * 1.5 + .25
-  // // spotLight2.intensity = Math.abs(Math.sin(elapsedTime)) + 1
-  // // spotLight2.position.z = Math.sin(elapsedTime) * 1.5
-  // spotLight3.position.y = Math.sin(elapsedTime) * 1.5 + .5
-  // spotLight3.position.z = Math.cos(elapsedTime) * 1.5 + .5
-  // // spotLight3.intensity = Math.abs(Math.sin(elapsedTime)) + 1.5
-  // // spotLight3.position.z = Math.sin(elapsedTime) * 1.5
 
   // Update controls
   controls.update()
